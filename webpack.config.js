67109a1cf2ce444631099f8277442faccb407bcf
Boris Glass <boris@glass.li>
2018-07-18 11:30:20 +0200

    css-loader and style-loader working

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ { loader: 'style-loader' },
+               { loader: 'css-loader',
+                  options: {
+                    modules: true
+            }
+          }
+        ]
+      }
+    ]
   }
+
 };
